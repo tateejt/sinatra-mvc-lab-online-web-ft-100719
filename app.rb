d6d165ac70c67aa86c1435be1f4a800c54941573
Emery Tate <tateejt@gmail.com>
2019-12-03 02:23:13 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -1,4 +1,13 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+  get '/' do 
+    erb :user_input
+  end
+  
+  post '/piglatinize' do 
+    pl = PigLatinizer.new 
+    
+    
+  end
 end
